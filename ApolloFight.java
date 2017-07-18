ffad5cdf0ea66f875b0677d026318feddc029777
ssoegriemsingh <ssoegriemsingh@qualogy.com>
2017-07-18 10:10:51 +0200

    Added Debug class for overall debugging.

---
+++ ApolloFight.java
@@ -0,0 +1,17 @@
+// Import:
+import packages.Debug;
+
+// Class:
+public class ApolloFight {
+	
+	// Constants:
+	private static final Debug DEBUG = new Debug();
+	
+	
+	// Main:
+	public static void main(String[] args) {
+		DEBUG.log("Test");
+	}
+	
+	
+}
